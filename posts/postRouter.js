7a510bbd5e98739de6e6bb856e82e7de3b2b601b
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-03 15:58:00 -0800

    added functionality to get for post

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const Post = require('../posts/postDb');
+const { getById } = require('../users/userDb');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   // do your magic!
   try {
-        const getPosts = await Post.get(req.body);
-        res.json(getPosts);
-  } catch (err){
+    const getPosts = await Post.get(req.body);
+    res.json(getPosts);
+  } catch (err) {
     res.json(error.message);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  try {
+    const getPostById = await Post.getById(id);
+    res.json(getPostById);
+  } catch (err) {
+    res.json(err.message);
+  }
 });
 
 router.delete('/:id', (req, res) => {
